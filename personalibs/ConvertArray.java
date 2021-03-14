9bf6e0526a9dae9c5878b7092978da071644166f
zen <klthree@users.noreply.github.com>
2021-03-13 19:39:50 -0500

    Added ConvertArray.java, modified .gitignore
    ConvertArray.java converts user supplied arrays to ArrayLists. Input not
    validated; use at own risk.
    .gitignore modified to include .class files

---
+++ personalibs/ConvertArray.java
@@ -0,0 +1,112 @@
+package personalibs;
+
+/**
+ * Converts arrays to ArrayLists. All types accomodated.
+ * Input not validated.
+ */
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class ConvertArray <E> {
+
+    public static ArrayList<Integer> toArray1d(int[] arr) {
+        ArrayList<Integer> intList = new ArrayList<>(arr.length);
+
+        for (int i = 0; i < arr.length; i++) {
+            intList.add(arr[i]);
+        }
+
+        return intList;
+    }
+
+    public static ArrayList<Double> toArray1d(double[] arr) {
+        ArrayList<Double> dblList = new ArrayList<>(arr.length);
+
+        for (int i = 0; i < arr.length; i++) {
+            dblList.add(arr[i]);
+        }
+
+        return dblList;
+    }
+
+    public static ArrayList<Boolean> toArray1d(boolean[] arr) {
+        ArrayList<Boolean> boolList = new ArrayList<>(arr.length);
+
+        for (int i = 0; i < arr.length; i++) {
+            boolList.add(arr[i]);
+        }
+
+        return boolList;
+    }
+
+    public static ArrayList<Character> toArray1d(char[] arr) {
+        ArrayList<Character> charList = new ArrayList<>(arr.length);
+
+        for (int i = 0; i < arr.length; i++) {
+            charList.add(arr[i]);
+        }
+
+        return charList;
+    }
+
+    public static ArrayList<Short> toArray1d(short[] arr) {
+        ArrayList<Short> shortList = new ArrayList<>(arr.length);
+
+        for (int i = 0; i < arr.length; i++) {
+            shortList.add(arr[i]);
+        }
+
+        return shortList;
+    }
+
+    public static ArrayList<Long> toArray1d(long[] arr) {
+        ArrayList<Long> longList = new ArrayList<>(arr.length);
+
+        for (int i = 0; i < arr.length; i++) {
+            longList.add(arr[i]);
+        }
+
+        return longList;
+    }
+
+    public static ArrayList<Byte> toArray1d(byte[] arr) {
+        ArrayList<Byte> byteList = new ArrayList<>(arr.length);
+
+        for (int i = 0; i < arr.length; i++) {
+            byteList.add(arr[i]);
+        }
+
+        return byteList;
+    }
+
+    public static ArrayList<String> toArray1d(String[] arr) {
+        ArrayList<String> stringList = new ArrayList<>(arr.length);
+
+        for (int i = 0; i < arr.length; i++) {
+            stringList.add(arr[i]);
+        }
+
+        return stringList;
+    }
+
+    public static ArrayList<Float> toArray1d(float[] arr) {
+        ArrayList<Float> floatList = new ArrayList<>(arr.length);
+
+        for (int i = 0; i < arr.length; i++) {
+            floatList.add(arr[i]);
+        }
+
+        return floatList;
+    }
+
+    public static <E> ArrayList<E> toArray1d(E[] arr) {
+        ArrayList<E> elementList = new ArrayList<>(arr.length);
+
+        for (int i = 0; i < arr.length; i++) {
+            elementList.add(arr[i]);
+        }
+
+        return elementList;
+    }
+}
